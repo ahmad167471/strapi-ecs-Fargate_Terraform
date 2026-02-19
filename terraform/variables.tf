e882105aed16d19d871481d358d9f3fea30b3975
Ahmad Amin <ahmad167471.com>
2026-02-19 18:36:34 +0530

    Fix variables.tf: add aws_region, aws_account_id, strapi_db_password

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,25 @@
+# AWS region to deploy resources
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
+
+# AWS account ID
+variable "aws_account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
+
+# Strapi database password (sensitive)
+variable "strapi_db_password" {
+  description = "Password for Strapi database"
+  type        = string
+  sensitive   = true
+}
+
 # Docker image tag for ECS deployment
 variable "image_tag" {
   description = "Tag of the Docker image to deploy"
   type        = string
-  default     = "latest"
 }
+
