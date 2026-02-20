ef539b4b5e19b51ae7357b369dd7829c89b72c94
Ahmad Amin <ahmad167471.com>
2026-02-20 11:51:29 +0530

    Fix ECR config

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,12 +1,26 @@
+########################################
+# ECR Repository
+########################################
+
 resource "aws_ecr_repository" "repo" {
-  name = "strapi-ecs"
+  name                 = "strapi-ecs-ahmad"
+  image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+
+  tags = {
+    Name = "strapi-ecs-ahmad"
+  }
 }
+
+########################################
+# ECS Task Definition
+########################################
+
 resource "aws_ecs_task_definition" "strapi" {
-  family                   = "strapi-task"
+  family                   = "strapi-task-ahmad"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "512"
@@ -18,24 +32,29 @@ resource "aws_ecs_task_definition" "strapi" {
   container_definitions = jsonencode([
     {
       name      = "strapi"
-      image     = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/strapi-ecs:${var.image_tag}"
+      image     = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/strapi-ecs-ahmad:${var.image_tag}"
       essential = true
 
       portMappings = [
         {
           containerPort = 1337
           hostPort      = 1337
+          protocol      = "tcp"
         }
       ]
 
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = "/ecs/strapi"
+          awslogs-group         = "/ecs/strapi-ahmad"
           awslogs-region        = var.aws_region
           awslogs-stream-prefix = "ecs"
         }
       }
     }
   ])
+
+  tags = {
+    Name = "strapi-task-ahmad"
+  }
 }
